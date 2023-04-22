ddefd03734afe36e37dcfb0d0e42286359784a00
santoshPagire <santoshpagire1007@gmail.com>
2023-04-22 14:27:40 +0530

    fileUpDate

--- welcome.java
+++ welcome.java
@@ -5,6 +5,7 @@ public class welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("WELCOME TO JAVA PROGRAME");
+System.out.println("CHANGES IN JAVA PROGRAME");
 	}
 
 }
